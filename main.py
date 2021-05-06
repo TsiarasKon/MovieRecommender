4512fcc4b426afe5176d83850611619bcc085831
Michael Bzms <35953365+michaelbzms@users.noreply.github.com>
2021-05-06 14:17:42 +0300

    Added rest of basic features + save/load functionality

--- main.py
+++ main.py
@@ -7,63 +7,94 @@ from rdflib import Graph, Literal, URIRef, Namespace, XSD
 imdb_data_folder = 'imdb_data/'
 name_basics_file = 'name.basics.tsv'
 tconst_files = [
-    'title.basics.tsv',
+    ('title.basics.tsv', None),
     # 'title.crew.tsv',
     # 'title.episode.tsv',
-    # 'title.principals.tsv',
-    'title.ratings.tsv'
+    # ('title.principals.tsv', ['tconst', 'nconst', 'category']),
+    ('title.ratings.tsv', None)
 ]
-
 movielens_data_folder = 'movielens_data/'
 
 pd.set_option('display.max_columns', None)
+
+
+# NAMESPACES
+ns_movies = Namespace('https://www.imdb.com/title/')
+ns_principals = Namespace('https://www.imdb.com/name/')
+ns_predicates = Namespace('http://example.org/props/')
 
 
 def data_loading():
     # load each file with tconst (title id) as index
     print('Loading IMDB data')
     all_dfs = []
-    for file in tconst_files:
-        df = pd.read_csv(imdb_data_folder + file, index_col='tconst',  # usecols=[], dtype={'col': 'UInt32'}
+    for file, usecols in tconst_files:
+        df = pd.read_csv(imdb_data_folder + file, index_col='tconst',  usecols=usecols,
                          sep='\t', encoding='utf-8',
                          keep_default_na=False, na_values=['\\N'])
         all_dfs.append(df)
 
     # combine all into one big fat DataFrame
     print('concatenating...')
-    full_df = pd.concat(all_dfs, axis=1)
-    full_df = full_df[(full_df['titleType'].isin(['tvMovie', 'movie'])) &
-                      (full_df['startYear'] >= 1960) & (full_df['startYear'] <= 2020)]
+    movies_df = pd.concat(all_dfs, axis=1)
+    movies_df = movies_df[(movies_df['titleType'].isin(['tvMovie', 'movie'])) &
+                          (movies_df['startYear'] >= 1975) & (movies_df['startYear'] <= 2020)]
     print('done')
 
     # fix NA and types afterwards as it is not supported for read_csv
-    full_df['numVotes'] = full_df['numVotes'].fillna(0).astype(np.uint16)
-    full_df['isAdult'] = full_df['isAdult'].astype(np.bool)
-    full_df['startYear'] = full_df['startYear'].fillna(0).astype(np.uint16)
-    full_df['endYear'] = full_df['endYear'].fillna(0).astype(np.uint16)
-    full_df['genres'] = full_df['genres'].fillna('').astype(np.str)
+    movies_df['numVotes'] = movies_df['numVotes'].fillna(0).astype(np.uint16)
+    movies_df['isAdult'] = movies_df['isAdult'].astype(np.bool)
+    movies_df['startYear'] = movies_df['startYear'].fillna(0).astype(np.uint16)
+    movies_df['endYear'] = movies_df['endYear'].fillna(0).astype(np.uint16)
+    movies_df['genres'] = movies_df['genres'].fillna('').astype(np.str)
 
     # filtering
-    full_df = full_df[(full_df['numVotes'] >= 500) &
-                      (~(full_df['genres'].str.contains('Short', regex=False, na=False))) &
-                      (full_df['genres'].str != '')]
+    movies_df = movies_df[(movies_df['numVotes'] >= 500) &
+                      (~(movies_df['genres'].str.contains('Short', regex=False, na=False))) &
+                      (movies_df['genres'].str != '')]
 
-    print(full_df)
-    return full_df
+    print('Loading edges')
+    principals_df = pd.read_csv(imdb_data_folder + 'title.principals.tsv',
+                             sep='\t',
+                             encoding='utf-8',
+                             keep_default_na=False,
+                             na_values=['\\N'],
+                             index_col='tconst',
+                             usecols=['tconst', 'nconst', 'category'])
+    principals_df = principals_df[principals_df.index.isin(movies_df.index)]
+    principals_df = principals_df[principals_df['category'].isin(['actor', 'actress', 'writer', 'director', 'composer'])]  # TODO: change if more roles
+
+    print(movies_df)
+    print(principals_df)
+
+    # TODO: do we need these?
+    # print('loading people info')
+    # people = pd.read_csv(imdb_data_folder + 'name.basics.tsv',
+    #                      sep='\t',
+    #                      encoding='utf-8',
+    #                      keep_default_na=False,
+    #                      na_values=['\\N'],
+    #                      usecols=['nconst', 'primaryName'])  # TODO: need more?
+    # filtered = people[people['nconst'].isin(movies_df.get_level_values('nconst'))]
+    # people_df = pd.DataFrame(index=filtered['nconst'], data={'name': list(filtered['primaryName'])})
+
+    return movies_df, principals_df
 
 
-# def build_rdf():
-if __name__ == '__main__':
-    df = data_loading()
-
-    ns_movies = Namespace('https://www.imdb.com/title/')
-    ns_predicates = Namespace('http://example.org/props/')
-    ns_xsd = Namespace('http://www.w3.org/2001/XMLSchema#')
+def build_and_save_rdf():
+    movies_df, principals_df = data_loading()
 
     g = Graph()
 
-    for imdb_id, data in tqdm(df.iterrows(), total=df.shape[0]):
+    for imdb_id, data in tqdm(movies_df.iterrows(), total=movies_df.shape[0]):
         movie = URIRef(ns_movies + imdb_id)
+
+        # find artists involved
+        try:
+            artists = principals_df.loc[imdb_id]
+        except KeyError:
+            # if there are not any noteworthy artists involved then it's probably not a very good film and should be ignored
+            continue
 
         # add genres
         has_genre = URIRef(ns_predicates + 'hasGenre')
@@ -73,19 +104,64 @@ if __name__ == '__main__':
             genre = URIRef(ns_movies + genre_str)
             g.add((movie, has_genre, genre))
 
-        # add years
+        # add year
         year = Literal(str(data['startYear']), datatype=XSD.integer)
         has_year = URIRef(ns_predicates + 'hasYear')
         g.add((movie, has_year, year))
 
+        # add IMDb rating and votes
+        rating = Literal(str(data['averageRating']), datatype=XSD.float)
+        has_rating = URIRef(ns_predicates + 'hasRating')
+        g.add((movie, has_rating, rating))
+        votes = Literal(str(data['numVotes']), datatype=XSD.integer)
+        has_votes = URIRef(ns_predicates + 'hasVotes')
+        g.add((movie, has_votes, votes))
+
+        # add artists such as actors, directors, etc
+        for artist, category in zip(artists['nconst'], artists['category']):
+            pred = None
+            if category == 'actor' or category == 'actress':
+                pred = URIRef(ns_predicates + 'hasActor')
+            elif category == 'director':
+                pred = URIRef(ns_predicates + 'hasDirector')
+            elif category == 'writer':
+                pred = URIRef(ns_predicates + 'hasWriter')
+            elif category == 'composer':
+                pred = URIRef(ns_predicates + 'hasComposer')
+            # TODO: editor and cinematographer?
+            if pred is not None:
+                g.add((movie, pred, URIRef(ns_principals + artist)))
+
+        # TODO: are info for artists useful for our task? Should we add such triples?
+
+    # Save graph
+    print('Saving graph...')
+    g.serialize(destination='movies.nt', format='nt')
+
+    # To load: g.parse('movies.nt',format='nt')
+
+    return g
+
+
+def load_rdf():
+    g = Graph()
+    return g.parse('movies.nt',format='nt')
+
+
+if __name__ == '__main__':
+    print('Loading rdf...')
+    g = load_rdf()
+    print('done')
+
     qres = g.query(
-        """SELECT DISTINCT ?x
+        """SELECT DISTINCT ?x ?year
            WHERE {
-              ?x pred:hasGenre ns:Drama .
-              ?x pred:hasYear 2003 .
-           }""", initNs={'ns': ns_movies, 'pred': ns_predicates})
+              ?x pred:hasGenre ns:Comedy .
+              ?x pred:hasYear ?year .
+              ?x pred:hasActor principals:nm0000120 .
+              FILTER(?year >= 2010)
+           }""", initNs={'ns': ns_movies, 'pred': ns_predicates, 'principals': ns_principals})
 
     print(f'Results: {len(qres)}')
     for row in qres:
-        # print("%s has genre 'Drama'" % row)
         print(row)
