24b205e3bd6d67fdea98d411de8d24996d1d7e83
Konstantinos Tsiaras <tsiaraskon@gmail.com>
2021-05-27 17:04:34 +0300

    UI: Added short timer to prevent API request spamming

--- movie-recommender-ui/src/components/recommendations.tsx
+++ movie-recommender-ui/src/components/recommendations.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import { Card, Carousel, Skeleton, Spin, Tag } from 'antd';
 import Movie from '../types';
 import { chunks } from '../utils';
@@ -7,31 +7,36 @@ import '../styles/recommendations.css';
 const RecommendationsComponent = ({ ratedMovies, allMovies }: { ratedMovies: Movie[], allMovies: Movie[] }) => {
   const [recommendedMovies, setRecommendedMovies] = useState<Movie[]>([]);
   const [responsesWaitingNum, setResponsesWaitingNum] = useState(0);
+  const timerRef = useRef<any>(null);     // timer used to prevent multiple back to back API requests
+  const timerDuration = 2000;
 
   const getRecommendations = useCallback((): void => {
     if (!ratedMovies.length) return;
-    setResponsesWaitingNum(prev => prev + 1);
-    const requestOptions = {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({
-        movieRatings: ratedMovies.map(m => ({ tconst: m.tconst, userRating: m.userRating })),
-        recommendationsNum: 10
-      })
-    };
-    console.log("Sending request with body: ", requestOptions.body)
-    fetch('/recommend', requestOptions)
-      .then(response => response.json())
-      .then(data => {
-        console.log("Got response: ", data);
-        const recommendedTconstArr = data.map((el: any) => el.tconst);
-        setRecommendedMovies(allMovies.filter(m => recommendedTconstArr.includes(m.tconst)));
-        setResponsesWaitingNum(prev => prev - 1);
-      })
-      .catch(error => {
-        console.log("Got error: ", error);
-        setResponsesWaitingNum(prev => prev - 1);
-      });
+    clearTimeout(timerRef.current);
+    timerRef.current = setTimeout(() => {
+      setResponsesWaitingNum(prev => prev + 1);
+      const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          movieRatings: ratedMovies.map(m => ({ tconst: m.tconst, userRating: m.userRating })),
+          recommendationsNum: 10
+        })
+      };
+      console.log("Sending request with body: ", requestOptions.body)
+      fetch('/recommend', requestOptions)
+        .then(response => response.json())
+        .then(data => {
+          console.log("Got response: ", data);
+          const recommendedTconstArr = data.map((el: any) => el.tconst);
+          setRecommendedMovies(allMovies.filter(m => recommendedTconstArr.includes(m.tconst)));
+          setResponsesWaitingNum(prev => prev - 1);
+        })
+        .catch(error => {
+          console.log("Got error: ", error);
+          setResponsesWaitingNum(prev => prev - 1);
+        });
+    }, timerDuration)
   }, [ratedMovies, allMovies]);
 
   useEffect(() => getRecommendations(), [getRecommendations]);
@@ -39,7 +44,7 @@ const RecommendationsComponent = ({ ratedMovies, allMovies }: { ratedMovies: Mov
   const recommendedMoviesCarouselCards = ([...chunks(recommendedMovies, 4)] as Movie[][]).map(mArr =>
     <div>
       {mArr.map(m =>
-        <Card title={m.primaryTitle} bordered={true}>
+        <Card title={m.primaryTitle} bordered={true} key={m.tconst}>
           Year: <strong>{m.startYear}</strong>
           <br />
           Rating: <strong>{m.averageRating}</strong> <i>({m.numVotes} votes)</i>
@@ -58,6 +63,7 @@ const RecommendationsComponent = ({ ratedMovies, allMovies }: { ratedMovies: Mov
     <Card
       title={<Skeleton title paragraph={false} loading={!!responsesWaitingNum} active>{m.primaryTitle}</Skeleton>}
       bordered={true} className="movie-card"
+      key={m.tconst}
     >
       <Skeleton title={false} paragraph={{ rows: 2 }} loading={!!responsesWaitingNum} active>
         Year: <strong>{m.startYear}</strong>
